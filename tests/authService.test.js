1549d09bf06fe20d71cbe0952ed19da17e28bf9d
DanKalh <92654593+DanKalh@users.noreply.github.com>
2024-07-26 22:48:34 -0700

    Update authService.test.js

--- tests/authService.test.js
+++ tests/authService.test.js
@@ -1,5 +1,5 @@
 import { register, login } from '../backend/authService';
-import { query } from '../backend/db';
+import pool from '../backend/db';
 import bcrypt from 'bcrypt';
 
 jest.mock('../backend/db', () => ({
@@ -12,14 +12,14 @@ describe('Auth Service', () => {
   });
 
   it('should register a new user Paul Atreides', async () => {
-    query.mockResolvedValueOnce({ rows: [{ id: 1, email: 'paul.atreides@arrakis.com' }] });
+    pool.query.mockResolvedValueOnce({ rows: [{ id: 1, email: 'paul.atreides@arrakis.com' }] });
     const user = await register('paul.atreides@arrakis.com', 'MuadDib');
     expect(user).toHaveProperty('id');
     expect(user.email).toBe('paul.atreides@arrakis.com');
   });
 
   it('should register a new user Lisan Al Gaib', async () => {
-    query.mockResolvedValueOnce({ rows: [{ id: 2, email: 'lisan.al.gaib@arrakis.com' }] });
+    pool.query.mockResolvedValueOnce({ rows: [{ id: 2, email: 'lisan.al.gaib@arrakis.com' }] });
     const user = await register('lisan.al.gaib@arrakis.com', 'KwizatzHaderach');
     expect(user).toHaveProperty('id');
     expect(user.email).toBe('lisan.al.gaib@arrakis.com');
@@ -27,7 +27,7 @@ describe('Auth Service', () => {
 
   it('should login a user Paul Atreides', async () => {
     const hashedPassword = await bcrypt.hash('MuadDib', 10);
-    query.mockResolvedValueOnce({ rows: [{ id: 1, email: 'paul.atreides@arrakis.com', password: hashedPassword }] });
+    pool.query.mockResolvedValueOnce({ rows: [{ id: 1, email: 'paul.atreides@arrakis.com', password: hashedPassword }] });
     const user = await login('paul.atreides@arrakis.com', 'MuadDib');
     expect(user).toHaveProperty('id');
     expect(user.email).toBe('paul.atreides@arrakis.com');
@@ -35,7 +35,7 @@ describe('Auth Service', () => {
 
   it('should login a user Lisan Al Gaib', async () => {
     const hashedPassword = await bcrypt.hash('KwizatzHaderach', 10);
-    query.mockResolvedValueOnce({ rows: [{ id: 2, email: 'lisan.al.gaib@arrakis.com', password: hashedPassword }] });
+    pool.query.mockResolvedValueOnce({ rows: [{ id: 2, email: 'lisan.al.gaib@arrakis.com', password: hashedPassword }] });
     const user = await login('lisan.al.gaib@arrakis.com', 'KwizatzHaderach');
     expect(user).toHaveProperty('id');
     expect(user.email).toBe('lisan.al.gaib@arrakis.com');
